96dcde48750fef0f9cdc51552ac7f360ba9b7838
Aman Jain <97128126+AmanJain2401@users.noreply.github.com>
2022-06-12 22:53:11 +0530

    Update settings.py

--- InventoryManagementProject/settings.py
+++ InventoryManagementProject/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bx!si$v+)smaf!xv==&7k)gl8n4_4jzgl3@wh0^b_0c8l-2_h_'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
